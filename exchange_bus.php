d34af5be499ccbde264a9432b5cf32192c18db11
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-06-01 00:29:32 +0300

    Коррекция маршрутных ссылок

--- exchange_bus.php
+++ exchange_bus.php
@@ -3,7 +3,7 @@
 Plugin Name: ExchangeBus
 Plugin URI: https://github.com/wolzogen/exchange_bus
 Description: Шина синхронизации данных
-Version: 2.0.1
+Version: 2.0.2
 Author: wolzogen
 */
 
@@ -13,13 +13,13 @@ add_action('admin_menu', 'exchange_bus_add_pages');
 function exchange_bus_add_pages()
 {
     // Добавление меню главной страницы модуля:
-    add_menu_page('ExchangeBus', 'ExchangeBus', 7, __FILE__, 'exchange_bus_page', 'dashicons-filter');
+    add_menu_page('ExchangeBus', 'ExchangeBus', 7, 'exchange_bus_module', 'module_page', 'dashicons-filter');
     // Добавление субменю страницы конфигурации модуля:
-    add_submenu_page(__FILE__, 'Configuration', 'Конфигурация', 8, 'exchange_bus_configuration', 'configuration_page');
+    add_submenu_page('exchange_bus_module', 'Configuration', 'Конфигурация', 8, 'exchange_bus_configuration', 'configuration_page');
 }
 
 // Функция контента для страниы exchange_bus_page
-function exchange_bus_page()
+function module_page()
 {
     echo "<h2>Шина синхронизации данных</h2>";
 }
