e77791d3d20f40e15827be4dc12e126667bfa99c
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-05-31 23:44:29 +0300

    Создание главной страницы и страницы конфигурации модуля

--- exchange_bus.php
+++ exchange_bus.php
@@ -3,7 +3,29 @@
 Plugin Name: ExchangeBus
 Plugin URI: https://github.com/wolzogen/ExchangeBus
 Description: Шина синхронизации данных
-Version: 1.0.0
+Version: 2.0.0
 Author: wolzogen
 */
-?>
+
+add_action('admin_menu', 'exchange_bus_add_pages');
+
+// Функция добавления страниц
+function exchange_bus_add_pages()
+{
+    // Добавление меню главногой страницы модуля:
+    add_menu_page('ExchangeBus', 'ExchangeBus', 7, __FILE__, 'exchange_bus_page', 'dashicons-filter');
+    // Добавление субменю страницы конфигурации модуля:
+    add_submenu_page(__FILE__, 'Configuration', 'Конфигурация', 8, 'exchange_bus_configuration', 'configuration_page');
+}
+
+// Функция контента для страниы exchange_bus_page
+function exchange_bus_page()
+{
+    echo "<h2>Шина синхронизации данных</h2>";
+}
+
+// Функция контента для страниы configuration_page
+function configuration_page()
+{
+    echo "<h2>Конфигурация</h2>";
+}
