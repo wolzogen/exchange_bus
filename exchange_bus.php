915f98d2a8b7423433b1a21392c4bf020d41684e
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-06-02 00:19:21 +0300

    Более гибкая проверка существования файлов

--- exchange_bus.php
+++ exchange_bus.php
@@ -8,8 +8,8 @@ Author: wolzogen
 */
 
 // Установление константы с названием файла для синхронизации
-if (!defined('EXCHANGE_BUS_SYNC_FILE'))
-    define('EXCHANGE_BUS_SYNC_FILE', 'catalog.csv');
+if (!defined('EXCHANGE_BUS_CSV_FILE'))
+    define('EXCHANGE_BUS_CSV_FILE', 'catalog.csv');
 
 add_action('admin_menu', 'exchange_bus_add_pages');
 
@@ -17,25 +17,52 @@ add_action('admin_menu', 'exchange_bus_add_pages');
 function exchange_bus_add_pages()
 {
     // Добавление меню главной страницы модуля:
-    add_menu_page('ExchangeBus', 'ExchangeBus', 7, 'exchange_bus_module', 'module_page', 'dashicons-filter');
-    // Добавление субменю страницы конфигурации модуля:
-    add_submenu_page('exchange_bus_module', 'Configuration', 'Конфигурация', 8, 'exchange_bus_configuration', 'configuration_page');
+    add_menu_page('ExchangeBus', 'ExchangeBus', 7, 'exchange_bus_module', 'exchange_bus_module_page', 'dashicons-filter');
+    // Добавление субменю страницы импорта csv файла:
+    add_submenu_page('exchange_bus_module', 'CSV Import', 'CSV Import', 1, 'exchange_bus_csv', 'exchange_bus_csv_page');
+    // Добавление субменю страницы импорта xls файла:
+    add_submenu_page('exchange_bus_module', 'XLS Import', 'XLS Import', 2, 'exchange_bus_xls', 'exchange_bus_xls_page');
 }
 
-// Функция контента для страниы exchange_bus_page
-function module_page()
+// Функция контента для страниы exchange_bus_module_page
+function exchange_bus_module_page()
 {
     echo "<h2>Шина синхронизации данных</h2>";
 
-    // Проверяем наличие файла для синхронизации
-    if (!file_exists(get_home_path() . EXCHANGE_BUS_SYNC_FILE)) {
-        echo '<h4>Файл ' . EXCHANGE_BUS_SYNC_FILE . ' не найден</h4>';
-        return;
+    try {
+        outputFileDetails(EXCHANGE_BUS_CSV_FILE);
+    } catch (LogicException $e) {
+        echo $e->getMessage();
     }
 }
 
-// Функция контента для страниы configuration_page
-function configuration_page()
+/**
+ * Информация о файле
+ *
+ * @param string $filename
+ * @throws LogicException
+ */
+function outputFileDetails($filename)
 {
-    echo "<h2>Конфигурация</h2>";
+    $filepath = get_home_path() . $filename;
+
+    if (!file_exists($filepath)) {
+        throw new LogicException('<h4>Файл ' . $filename . ' не найден</h4>');
+    }
+
+    echo '<h4>Файл ' . $filename . ' найден</h4>';
+    echo 'Дата создания файла: ' . date("Y-d-m H:i:s.", filemtime($filepath));
 }
+
+// Функция контента для страниы exchange_bus_csv_page
+function exchange_bus_csv_page()
+{
+    echo "<h2>CSV Import</h2>";
+}
+
+// Функция контента для страниы exchange_bus_xls_page
+function exchange_bus_xls_page()
+{
+    echo "<h2>XLS Import</h2>";
+}
+
