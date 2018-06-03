64b84cbd968e120dedb0120152bf113b430da429
Elias Wolzogen <elias.wolzogen@yandex.ru>
2018-06-03 15:28:47 +0300

    Создание fileDescriptor объекта, создание функции importCsv

--- exchange_bus.php
+++ exchange_bus.php
@@ -11,6 +11,19 @@ Author: wolzogen
 if (!defined('EXCHANGE_BUS_CSV_FILE'))
     define('EXCHANGE_BUS_CSV_FILE', 'catalog.csv');
 
+// Определяем константу имени товара
+if (!defined('CSV_NAME'))
+    define('CSV_NAME', 'name');
+// Определяем константу артикула товара
+if (!defined('CSV_SKU'))
+    define('CSV_SKU', 'sku');
+// Определяем константу количества товара на складе
+if (!defined('CSV_STOCK'))
+    define('CSV_STOCK', 'stock');
+// Определяем константу цены товара
+if (!defined('CSV_PRICE'))
+    define('CSV_PRICE', 'price');
+
 add_action('admin_menu', 'exchange_bus_add_pages');
 
 // Функция добавления страниц
@@ -30,7 +43,7 @@ function exchange_bus_module_page()
     echo "<h2>Шина синхронизации данных</h2>";
 
     try {
-        outputFileDetails(EXCHANGE_BUS_CSV_FILE);
+        outputFileDetails(getFileDescriptor(EXCHANGE_BUS_CSV_FILE));
         echo '<br/><a href="/wp-admin/admin.php?page=exchange_bus_csv" class="button button-primary" style="margin-top: 10px;">' . __("Импортировать") . '</a>';
     } catch (LogicException $e) {
         echo $e->getMessage();
@@ -38,27 +51,37 @@ function exchange_bus_module_page()
 }
 
 /**
- * Информация о файле
+ * Формирование файлового дескриптора
  *
  * @param string $filename
- * @throws LogicException
+ * @return stdClass
  */
-function outputFileDetails($filename)
+function getFileDescriptor($filename)
 {
+    $fileDescriptor = new stdClass;
     $filepath = get_home_path() . $filename;
 
     if (!file_exists($filepath)) {
         throw new LogicException('<h4>Файл ' . $filename . ' не найден</h4>');
     }
 
-    echo '<h4>Файл ' . $filename . ' найден</h4>';
-    echo 'Дата создания файла: ' . date("Y-d-m H:i:s", filemtime($filepath));
+    $fileDescriptor->filepath = $filepath;
+    $fileDescriptor->filename = $filename;
+
+    return $fileDescriptor;
 }
 
 // Функция контента для страниы exchange_bus_csv_page
 function exchange_bus_csv_page()
 {
     echo "<h2>CSV Import</h2>";
+    try {
+        $fileDescriptor = getFileDescriptor(EXCHANGE_BUS_CSV_FILE);
+        outputFileDetails($fileDescriptor);
+        importCsv($fileDescriptor->filepath);
+    } catch (LogicException $e) {
+        echo $e->getMessage();
+    }
 }
 
 // Функция контента для страниы exchange_bus_xls_page
@@ -67,3 +90,52 @@ function exchange_bus_xls_page()
     echo "<h2>XLS Import</h2>";
 }
 
+/**
+ * Функция отображения информации о файле
+ *
+ * @param stdClass $fileDescriptor
+ */
+function outputFileDetails($fileDescriptor)
+{
+    echo '<h4>Файл ' . $fileDescriptor->filename . ' найден</h4>';
+    echo 'Дата создания файла: ' . date("Y-d-m H:i:s", filemtime($fileDescriptor->filepath));
+}
+
+/**
+ * @param string $filepath
+ */
+function importCsv($filepath)
+{
+    global $wpdb;
+
+    $csv = explode(PHP_EOL, file_get_contents($filepath));
+    $csvSize = count($csv);
+
+    for ($i = 0; $i < $csvSize; $i++) {
+        // Пропускаем итерацию, если строка является заголовком или пустой строкой
+        if ($i === 0 || empty($csv[0])) {
+            continue;
+        }
+
+        $csvLine = array_combine([CSV_NAME, CSV_SKU, CSV_STOCK, CSV_PRICE], str_getcsv($csv[$i], ';'));
+
+        $sqlPrepare = $wpdb->prepare("SELECT * FROM {$wpdb->postmeta} WHERE meta_key = %s AND meta_value = %d", '_sku', $csvLine[CSV_SKU]);
+        $postmetaSkuResults = $wpdb->get_results($sqlPrepare);
+
+        // Пропускаем итерацию, если количество записей с одинаковым _sku в поле meta_value более, чем 1
+        if (empty($postmetaResults) || count($postmetaResults) !== 1) {
+            continue;
+        }
+
+        // Устанавливаем указатель на первый элемент массива
+        $postmetaSku = reset($postmetaSkuResults);
+
+        $sqlPrepare = $wpdb->prepare("SELECT * FROM {$wpdb->postmeta} WHERE post_id = %d AND (meta_key = %s OR meta_key = %s)", $postmetaSku->post_id, '_stock', '_price');
+        $postmetaProductResults = $wpdb->get_results($sqlPrepare);
+
+        // Пропускаем итерацию, если не найдены записи текущего поста со значениями в meta_key = '_stock' и '_price'
+        if (empty($postmetaProductResults) || count($postmetaProductResults) !== 2) {
+            continue;
+        }
+    }
+}
